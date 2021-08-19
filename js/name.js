a4f456821abeb18638f4bf57f12597308411ec0d
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2021-08-19 11:53:34 +0100

    Update name.js

--- js/name.js
+++ js/name.js
@@ -9,6 +9,6 @@ function btnClick(e){
     btn.style.backgroundColor='black';
     btn.style.color='rgb(43, 200, 248)';
     btn.innerHTML="clicked";
-    output.innerHTML="<h2><b>Oluborode Akintunde Paul</b></h2> <br> <i>click here to view my</i> <a href='paulresume.html'>resume</a>";
+    output.innerHTML="<h2><b>Oluborode Akintunde Paul</b></h2> <br> <i>click here to view my</i> <a href='https://gifted-cori-9535e3.netlify.app'>resume</a>";
 console.log(output);
 }

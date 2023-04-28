379dd6021c11820a098c5b1b002c3ef31bed0bed
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-04-28 19:59:32 +0100

    Update name.js

--- js/name.js
+++ js/name.js
@@ -9,7 +9,8 @@ function btnClick(e){
     btn.style.backgroundColor='black';
     btn.style.color='rgb(43, 200, 248)';
     btn.innerHTML="clicked";
-    output.innerHTML="<h2><b>Oluborode Akintunde Paul</b></h2> <br> <i>click here to view my</i> <a href='http://my-project-link.vercel.app'>projects</a>";
+    output.innerHTML="<h2><b>  Hi, My name is Oluborode Akintunde Paul a 
+Software Developer experienced in developing and maintaining mobile/web applications with Flutter and Reactjs.  </b></h2> "<br> <i>click here to view my</i> <a href='http://my-project-link.vercel.app'>projects</a>";
     h1.textContent="Full Name";
     h1.style.fontFamily='monospace';
 console.log(output);
